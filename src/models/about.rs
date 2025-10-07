d809811ccf62a52efaba0e41630f7744763d3cd6
five <2974796278@@qq.com>
2025-10-07 16:44:23 +0800

    fix:项目地址

--- src/models/about.rs
+++ src/models/about.rs
@@ -12,7 +12,7 @@ pub mod about{
                 ui.label("egui版本: 0.32.3");
                 ui.label("rust版本: 1.90.0");
             });
-            ui.add(egui::github_link_file_line!("https://github.com/fengzhongsikao?tab=repositories", "项目github地址"));
+            ui.add(egui::github_link_file_line!("https://github.com/fengzhongsikao/boltshell", "项目github地址"));
             ui.add(egui::github_link_file_line!("https://github.com/TheBlindM/T-Shell", "借鉴T-Shell"));
             egui::Sides::new().show(
                 ui,
